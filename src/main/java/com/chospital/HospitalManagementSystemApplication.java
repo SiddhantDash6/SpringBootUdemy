03e8a19434a904e4105e9462703ed7bce1fa6ee0
siddhantdash98@gmail.com <siddhantdash98@gmail.com>
2024-03-18 00:27:15 +0530

    Second Commit

--- src/main/java/com/chospital/HospitalManagementSystemApplication.java
+++ src/main/java/com/chospital/HospitalManagementSystemApplication.java
@@ -3,6 +3,7 @@ package com.chospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
